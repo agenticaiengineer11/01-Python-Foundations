137fb671e9e5a63ef7c941d9e54019d776f95de3
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-07-31 09:20:10 +0500

    Employee project added using type alias

--- 01-Python-Foundations/exercises/practice_day27.py
+++ 01-Python-Foundations/exercises/practice_day27.py
@@ -24,6 +24,33 @@ student_info = {
     "CGPA": 3.63
 }
 def display(std:Student)->None:
-    print("Name",std["name"])
-    print("CGPA",std["CGPA"])
+    print("Name:",std["name"])
+    print("CGPA:",std["CGPA"])
 display(student_info)
+print("============Employee mini project===================")
+Employee : TypeAlias = dict[str,str | int | float]
+Employeelist = list[Employee]
+Employee_info = {
+    "name": "Ali",
+    "age" : 23,
+    "salary": 45000,
+    "department": "Marketing"
+}
+def add_employee(employee:Employee,employees:Employeelist) ->None:
+    name = input("Enter employee name: ")
+    age = int(input("Enter the age of employee: "))
+    salary = int(input("Enter the salary of the employee: "))
+    department = input("Enter the department of the employee: ")
+    for employee in Employeelist:
+        if employee["name"] == name:
+            print("Employee already exists")
+
+        emp = {
+            "name": name,
+            "age": age,
+            "salary": salary,
+            "department": department
+        }
+        Employeelist.append(emp)
+        print("Employee added successfully")
+        
