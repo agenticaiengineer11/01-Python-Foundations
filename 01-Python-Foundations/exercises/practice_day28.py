f4cccde38571242f61c4e79205f05c524096bc88
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-02 10:21:44 +0500

    Student class type hinting

--- 01-Python-Foundations/exercises/practice_day28.py
+++ 01-Python-Foundations/exercises/practice_day28.py
@@ -7,4 +7,14 @@ class Employee:
         print(f"Name: {self.name}, Salary: {self.salary}")
 emp  = Employee("Noman ALi", 50000)
 emp.display()
-        
+
+print("==========student custom class type hint===============")
+class Student:
+    def __init__(self,name:str,roll_no:int,CGPA:float):
+        self.name = name
+        self.roll_no = roll_no
+        self.CGPA = CGPA
+    def display(self)->None:
+        print(f"Name: {self.name}, Roll No: {self.roll_no}, CGPA: {self.CGPA}")
+std = Student("Noman",24,3.63)
+std.display()
