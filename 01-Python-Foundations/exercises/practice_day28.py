0713556dcf760836ce0660bd5cb2fffa88f05c0f
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-02 11:02:48 +0500

    Student data class added

--- 01-Python-Foundations/exercises/practice_day28.py
+++ 01-Python-Foundations/exercises/practice_day28.py
@@ -8,3 +8,10 @@ class Employee:
     salary: float
 emp = Employee("Noman",22,50000)
 print(emp)
+@dataclass
+class Student:
+    name:str
+    roll_no:int
+    cgpa:float
+std= Student("Noman",24,3.63)
+print(std)
