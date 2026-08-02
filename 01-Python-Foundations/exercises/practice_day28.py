72925f05b05b6d62717f5799061d044d511b8c04
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-02 10:25:39 +0500

    Bank account custom class type hint

--- 01-Python-Foundations/exercises/practice_day28.py
+++ 01-Python-Foundations/exercises/practice_day28.py
@@ -18,3 +18,14 @@ class Student:
         print(f"Name: {self.name}, Roll No: {self.roll_no}, CGPA: {self.CGPA}")
 std = Student("Noman",24,3.63)
 std.display()
+print("==============Bank acount custom class type hint===========")
+class BankAccount:
+    def __init__(self,account_number:str,account_holder:str,balance:float):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = balance
+    def display(self) ->None:
+        print(f"Account Number: {self.account_number}, Account holder: {self.account_holder}, Balance: {self.balance}")
+b = BankAccount("12234","Noman ALi",45000)
+
+b.display()
