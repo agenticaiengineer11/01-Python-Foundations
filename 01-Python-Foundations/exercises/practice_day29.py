37c28c0aa36daef30ebc3dcdaf00e00adefc4507
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-03 09:58:26 +0500

    User password hidden using repr= false

--- 01-Python-Foundations/exercises/practice_day29.py
+++ 01-Python-Foundations/exercises/practice_day29.py
@@ -1,6 +1,8 @@
 print("============Day 29 of Python Foundations==========")
 print("=============Advanced Data classes with field and default_factory========")
 from dataclasses import dataclass, field
+
+
 @dataclass
 class Employee:
     name:str 
@@ -11,3 +13,10 @@ emp1 = Employee("Noman",25, )
 print(emp)
 print(emp1)
 print(emp ==emp1)
+print("==========Hidden fields in data classes with repr = False=====")
+@dataclass
+class User:
+    username: str
+    password: str = field(repr =False)
+user = User("Noman","1244")
+print(user)
