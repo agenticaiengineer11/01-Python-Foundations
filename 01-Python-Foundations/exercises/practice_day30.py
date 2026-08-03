aa52b145df34ff380f268867d83b12c0ea89026c
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-03 10:48:41 +0500

    Order status shown using Enum classes

--- 01-Python-Foundations/exercises/practice_day30.py
+++ 01-Python-Foundations/exercises/practice_day30.py
@@ -5,5 +5,18 @@ class status(Enum):
     APPROVED = "approved"
     REJECTED = "rejected"
 order_status = status.PENDING
+if order_status == status.PENDING:
+    print("Order is pending")
 print(order_status.name)
 print(order_status.value)
+
+print("===========Enum classes of Order status=========")
+class OrderStatus(Enum):
+    PENDING = "pending"
+    PROCESSING = "processing"
+    SHIPPED = "shipped"
+    DELIVERED = "delivered"
+    CANCELED = "canceled"
+status = order_status.PENDING
+if status == order_status.PENDING:
+    print("order is pending")
