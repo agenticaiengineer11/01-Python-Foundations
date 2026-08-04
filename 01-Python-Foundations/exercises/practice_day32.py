0483edfec66799d9c7fb8f0d8f00cef2a104cd39
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-04 16:26:52 +0500

    Count words using collection counter module

--- 01-Python-Foundations/exercises/practice_day32.py
+++ 01-Python-Foundations/exercises/practice_day32.py
@@ -3,3 +3,13 @@ from collections import Counter
 text =  "Hello Noman"
 count = Counter(text)
 print(count)
+print("========counting words in list=======")
+from collections import Counter
+words = [
+    "apple",
+    "banana",
+    "apple",
+    "orange"
+]
+count  = Counter(words)
+print(count)
