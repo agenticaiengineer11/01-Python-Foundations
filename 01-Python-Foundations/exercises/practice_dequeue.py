6c4d33cd0e9c1b6b7afb9a5ebcc5e24ecb10cd87
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-05 10:04:57 +0500

    Dequeue named are appended

--- 01-Python-Foundations/exercises/practice_dequeue.py
+++ 01-Python-Foundations/exercises/practice_dequeue.py
@@ -2,3 +2,7 @@ print("=============Dequeue in python =============")
 from collections import deque
 numbers = deque([2,3,4,4])
 print(numbers)
+
+name = ["sara","noman"]
+name.append("ali")
+print(name)
