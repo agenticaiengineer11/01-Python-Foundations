77b265d08bcab8ef1c9426c60ed557b9be52d576
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-05 10:09:31 +0500

    Function of dequeue added

--- 01-Python-Foundations/exercises/practice_dequeue.py
+++ 01-Python-Foundations/exercises/practice_dequeue.py
@@ -2,7 +2,13 @@ print("=============Dequeue in python =============")
 from collections import deque
 numbers = deque([2,3,4,4])
 print(numbers)
+numbers.rotate(1)
+print(numbers)
 
-name = ["sara","noman"]
+name = deque(["sara","noman"])
 name.append("ali")
+print(name)
+name.pop()
+print(name)
+name.appendleft("muku")
 print(name)
