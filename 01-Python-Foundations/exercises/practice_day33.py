44240e37835c89fbff8d661b6034dbb7e771a678
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-05 10:30:29 +0500

    Real copy of list to another with different objects

--- 01-Python-Foundations/exercises/practice_day33.py
+++ 01-Python-Foundations/exercises/practice_day33.py
@@ -1,6 +1,16 @@
 print("===================Assignement(=),shallowcopy(copy()),deepcopy(deepcopy())")
 numbers = [2,34,54,53]
-numbers1 = numbers
+numbers1 = numbers #assignment operator
 numbers.append(40)
 print(numbers)
 print(numbers1)
+
+print("==========Real Copy===========")
+import copy
+numbers = [2,34,53,645,34]
+numbers2 = copy.copy(numbers)
+print(numbers)
+print(numbers2)
+numbers.append(40)
+print(numbers)
+print(numbers2)
