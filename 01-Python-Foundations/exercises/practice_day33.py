b713167f9245f0ce0bbd57ed7f7a7cc4df19d060
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-05 10:36:03 +0500

    Shallow copy added

--- 01-Python-Foundations/exercises/practice_day33.py
+++ 01-Python-Foundations/exercises/practice_day33.py
@@ -14,3 +14,12 @@ print(numbers2)
 numbers.append(40)
 print(numbers)
 print(numbers2)
+import copy
+student = [
+    "Noman",
+    [2,3,5,5]
+]
+student1= copy.copy(student)
+student1[1].append(100)
+print(student)
+print(student1)
